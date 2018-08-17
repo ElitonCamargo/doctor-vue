67a706ebb2ef26ebc42e97fc5369133357fa608b
darlanvieira <darlan.vieira2000@gmail.com>
2018-08-17 16:51:10 -0300

    Adicionado favcon, configurado searchbox, botao Login

--- src/components/Home/Index.vue
+++ src/components/Home/Index.vue
@@ -7,17 +7,10 @@
                 dark
             >
                 <v-flex xs10>
-                    <v-card>
-                        <v-card-media
-                            src="https://wallpaper.wiki/wp-content/uploads/2017/05/wallpaper.wiki-Beautiful-Full-HD-Images-PIC-WPE0011754.jpg"
-                            height="150vh"
-                        >
-                            
-                        </v-card-media>
-                    </v-card>
+                    CONTENT
                 </v-flex>
             </v-layout>
-        </v-container>        
+        </v-container>       
     </v-content>
 </template>
 
@@ -26,6 +19,7 @@ export default {
     name: "Home",
     data() {
         return {}
+            Logged: true
         }
     }
 </script>
