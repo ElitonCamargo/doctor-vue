80d5dbdec0a2f901c7780c1c2901360ab7f16911
darlanvieira <darlan.vieira2000@gmail.com>
2018-08-17 14:44:24 -0300

    Criado o prototipo de navbar

---
+++ src/components/Navigation/Toolbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-toolbar
+        color="blue darken-2"
+        flat
+        absolute
+        
+    >
+        <v-layout
+            align-center
+            justify-center
+        >
+            <v-flex md5 sm8>
+                <v-form 
+                >
+                    <v-text-field
+                        solo
+                        class="mt-1"
+                        label="Nome / CRM"
+                        append-icon="search"
+                    >
+                    </v-text-field>
+                </v-form>
+            </v-flex>
+        </v-layout>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'Toolbar'
+}
+</script>
