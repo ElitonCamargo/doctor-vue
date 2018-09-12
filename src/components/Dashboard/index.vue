00b52ab798f34e82438e7a8a3076ec8ca3d345fb
darlanvieira <darlan.vieira2000@gmail.com>
2018-09-12 17:20:38 -0300

    dash

---
+++ src/components/Dashboard/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-content>
+        <v-container
+            fluid
+            grid-list-lg
+        >
+            <v-layout wrap justify-center align-center>
+                <v-flex xs10>
+                    <v-card
+                        color="cyan"
+                        class="white--text"
+                    >
+                        <v-layout>
+                            <v-flex xs2>
+                                <v-img
+                                    :src="user.image"
+                                    height="25vh"
+                                    contain
+                                >
+                                </v-img>
+                            </v-flex>
+
+                        </v-layout>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+export default {
+    name: 'dashboard',
+    data(){
+        return{
+            hello: "HELLO WORLD",
+            user: {
+                id: 1,
+                email: "darlan.vieira",
+                name: "darlan",
+                ca: "2018",
+                image: "https://randomuser.me/api/portraits/men/32.jpg"
+            }
+        }
+    },
+    
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
+import Dashboard from '@/components/Dashboard'
 Vue.use(Router)
 
 export default new Router({
@@ -21,6 +22,11 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
